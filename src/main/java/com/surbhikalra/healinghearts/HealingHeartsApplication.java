ae91447180b5bdb5ade9a0ffa572b9fa509d0548
surbhi28kalra <surbhi28kalra@gmail.com>
2024-10-16 16:07:39 -0400

    Initial commit

---
+++ src/main/java/com/surbhikalra/healinghearts/HealingHeartsApplication.java
@@ -0,0 +1,13 @@
+package com.surbhikalra.healinghearts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealingHeartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealingHeartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surbhikalra/healinghearts/HealingHeartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surbhikalra.healinghearts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealingHeartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
