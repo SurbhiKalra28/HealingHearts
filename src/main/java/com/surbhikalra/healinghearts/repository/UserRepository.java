4ec9c65fbd8c5531d300c5ba66e797b98bd4682e
surbhi28kalra <surbhi28kalra@gmail.com>
2024-10-17 11:03:41 -0400

    [115] Start Junit testing code

--- src/main/java/com/surbhikalra/healinghearts/repository/UserRepository.java
+++ src/main/java/com/surbhikalra/healinghearts/repository/UserRepository.java
@@ -2,7 +2,15 @@ package com.surbhikalra.healinghearts.repository;
 
 import com.surbhikalra.healinghearts.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(@Param("email") String email);
+
 }
